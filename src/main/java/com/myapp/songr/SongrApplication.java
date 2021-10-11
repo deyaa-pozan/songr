3baccccd3db806aa37603f79a48e5a5980589901
deyaa-pozan <deyaa.pozan@gmail.com>
2021-10-11 03:19:57 +0300

    lab12

--- src/main/java/com/myapp/songr/SongrApplication.java
+++ src/main/java/com/myapp/songr/SongrApplication.java
@@ -1,5 +1,4 @@
 package com.myapp.songr;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SongrApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SongrApplication.class, args);
 	}
 
--- src/main/java/com/myapp/songr/model/Album.java
+++ src/main/java/com/myapp/songr/model/Album.java
@@ -1,6 +1,15 @@
 package com.myapp.songr.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private long id;
     private String title;
     private String artist;
     private int songCount;
